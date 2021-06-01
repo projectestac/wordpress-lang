770122e207e0c3734ecd5465365d2942c912a5ce
Toni Ginard <toni.ginard@gmail.com>
2021-06-01 19:08:48 +0200

    compile.sh: Minor improvements and added creation of required soft links

--- compile.sh
+++ compile.sh
@@ -2,9 +2,7 @@
 
 source config/settings.sh
 
-
-# -- Obtain command line parameters --------------------------------------------
-
+# Check command line parameters
 if [ $# -ne 1 ]; then
     echo "Usage: bash $0 <project>"
     exit 1
@@ -19,34 +17,71 @@ source config/settings-$1.sh
 
 PROJECT=$1
 
-pushd translations/$PROJECT/languages/
+pushd translations/"$PROJECT"/languages/ > /dev/null || exit
 
 for code in ${LANGS[@]}; do
     path="$code/"
 
-    wp i18n make-json $path/ --no-purge
-    wp i18n make-json $path/plugins/ --no-purge
-    wp i18n make-json $path/themes/ --no-purge
+    wp i18n make-json "$path"/ --no-purge
+    wp i18n make-json "$path"/plugins/ --no-purge
+    wp i18n make-json "$path"/themes/ --no-purge
 
-    find $path -name \*.po -print -execdir sh \
-        -c 'msgfmt -f -o "$(basename "$0" .po)-'"$code"'.mo" "$0"' '{}' \;
+    find "$path" -name \*.po -print -execdir sh -c 'msgfmt -f -o "$(basename "$0" .po)-'"$code"'.mo" "$0"' '{}' \;
 done
 
-mkdir ../binaries
+# Remove previous compilation
+rm -rf ../binaries/
+
+# Create directory structure required later
+mkdir -p ../binaries/mu-plugins/
 
 for code in ${LANGS[@]}; do
-    pushd $code/
-    mv wordpress-$code.mo $code.mo
+    pushd > /dev/null "$code/" || exit
+
+    mv wordpress-"$code".mo "$code".mo
     rename -v "s/^wordpress-(.+)$/$code-\$1/" wordpress-*.json
+
     cp -r --parents $(find -name \*.mo -type f -print) ../../binaries
     cp -r --parents $(find -name \*.json -type f -print) ../../binaries
     rm $(find -name \*.mo -type f -print)
     rm $(find -name \*.json -type f -print)
-    mkdir ../../binaries/mu-plugins/
-    mv ../../binaries/functions-*.mo ../../binaries/mu-plugins/
-    popd
+
+    popd > /dev/null || exit
 done
 
-popd
+mv ../binaries/functions-*.mo ../binaries/mu-plugins/
 
-echo -e "\n= DONE. Compiled translations at: $PROJECT/po/branch\n"
+# Create symbolic links
+pushd ../binaries/plugins/ > /dev/null || exit
+
+for code in ${LANGS[@]}; do
+    if test -f ../mu-plugins/functions-"$code".mo; then
+        ln -s ../mu-plugins/functions-"$code".mo agora-functions-"$code".mo
+    fi
+    if test -f ../mu-plugins/functions-"$code".mo; then
+        ln -s ../mu-plugins/functions-"$code".mo common-functions-"$code".mo
+    fi
+    if test -f bbpress-enable-tinymce-visual-tab-"$code".mo; then
+        ln -s bbpress-enable-tinymce-visual-tab-"$code".mo bbp-tinymce-visual-tab-"$code".mo
+    fi
+    if test -f buddypress-group-email-subscription-"$code".mo; then
+        ln -s buddypress-group-email-subscription-"$code".mo bp-ass-"$code".mo
+    fi
+    if test -f widget-visibility-without-jetpack-"$code".mo; then
+        ln -s widget-visibility-without-jetpack-"$code".mo jetpack-"$code".mo
+    fi
+    if test -f wp-recaptcha-"$code".mo; then
+        ln -s wp-recaptcha-"$code".mo recaptcha-"$code".mo
+    fi
+    if test -f tabs-responsive-"$code".mo; then
+        ln -s tabs-responsive-"$code".mo wpsm_tabs_r-"$code".mo
+    fi
+    if test -f wordpress-telegram-"$code".mo; then
+        ln -s wordpress-telegram-"$code".mo wptelegram-"$code".mo
+    fi
+done
+
+popd > /dev/null || exit
+popd > /dev/null || exit
+
+echo -e "\n= DONE. Compiled translations at: translations/$PROJECT/binaries/\n"
